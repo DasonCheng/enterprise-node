f1e2711dd58449fff94ce6b47e8c5f47724e3b38
Origin <dasoncheng@foxmail.com>
2016-05-24 17:48:10 +0800

    mongodb2mysql

--- routers/main.js
+++ routers/main.js
@@ -3,64 +3,46 @@
  */
 var express = require('express');
 var multer = require('multer');
-var mongoose = require('mongoose');
+var mysql = require('mysql');
 var router = express.Router();
-mongoose.connect('mongodb://jusheng:123aaAA@localhost:27017/jusheng');
-
-var companySchema = mongoose.Schema({
-    name: String,
-    tel: String,
-    post: String,
-    www: String,
-    logo: String,
-    intro_cn: String,
-    intro_en: String,
-    intro_title: String
-}, {collection: 'company'});
-var company = mongoose.model('company', companySchema);
-var companyData = null
-company.find().exec(function (err, data) {
-    if (err) return console.error("err");
-    companyData = data;
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'jusheng',
+    password: '123aaAA'
 });
-var bannerSchema = mongoose.Schema({
-    title: String,
-    words: String,
-    link: Object
-}, {collection: 'banner'});
-var banner = mongoose.model('banner', bannerSchema);
-
-var bannerData = null;
-banner.find().exec(function (err, date) {
-    if (err) return console.error("err");
-    bannerData = date;
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
 });
+connection.end();
+
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
 
 
-router.get('/', function (req, res) {
-    res.render('index', {companyData, bannerData});
+router.get('/', function(req, res) {
+    res.render('index', {});
 });
 
-router.get('/upload', function (req, res) {
+router.get('/upload', function(req, res) {
     res.render('upload', {});
 });
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, 'uploads/');
     },
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, 'Orange' + '-' + Date.now() + file.originalname.match(/\.[^\.]+$/)[0]);
     }
 });
-router.post('/upload', function (req, res) {
+router.post('/upload', function(req, res) {
     var upload = multer({
         storage: storage,
-        fileFilter: function (req, file, cb) {
+        fileFilter: function(req, file, cb) {
             if (file.mimetype.match(/^[^\/]+/)[0] == 'image') {
                 cb(null, true);
             } else {
@@ -68,10 +50,10 @@ router.post('/upload', function (req, res) {
                 cb(null, false);
             }
         },
-        limits: {fileSize: 1024 * 1024 * 2}
+        limits: { fileSize: 1024 * 1024 * 2 }
     }).single('photo');
 
-    upload(req, res, function (err) {
+    upload(req, res, function(err) {
         if (err) {
             // An error occurred when uploading
             console.log(err);
