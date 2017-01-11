8ae4f636f83132b7d06550e2b82cedf46f10392b
Origin <dasoncheng@yahoo.com>
2017-01-11 15:45:06 +0800

    update

--- src/components/pages/canvas-ball.page.vue
+++ src/components/pages/canvas-ball.page.vue
@@ -13,19 +13,28 @@
           x: null,
           y: null
         },
-        balls: null
+        BALLS: []
       }
     },
     computed: {},
     methods: {
       initBalls(){
-        let BALLS = new Array;
-        _.times(10, () => {
-          _.concat(BALLS,{
-            x:_.random(this.size.width,true),
-            y:_.random(this.size.width,true),
-            r:_.random(this.size.width,true)
+        _.times(100, () => {
+          this.BALLS = _.concat(this.BALLS, {
+            x: _.random(this.size.width, true),
+            y: _.random(this.size.height, true),
+            radius: _.sample([1, 4, 6, 8]),
+            vx: _.random(2, true) * _.sample([-1, 1]),
+            vy: _.random(2, true) * _.sample([-1, 1])
           })
+        });
+      },
+      renderArc(ctx){
+        this.BALLS.map((ball) => {
+          ctx.beginPath();
+          ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+          ctx.fill();
+          ctx.closePath();
         })
       }
     },
@@ -33,6 +42,7 @@
       const canvas = this.$el;
       const ctx = canvas.getContext('2d');
       this.initBalls();
+      this.renderArc(ctx);
     }
   }
 </script>
