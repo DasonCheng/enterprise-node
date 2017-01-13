4e01c4ec664ed41bdd2e9df19e79ea30f933f9e6
Origin <dasoncheng@yahoo.com>
2017-01-13 13:31:38 +0800

    update

--- src/components/pages/canvas-ball.page.vue
+++ src/components/pages/canvas-ball.page.vue
@@ -19,21 +19,41 @@
     computed: {},
     methods: {
       initBalls(){
-        _.times(100, () => {
-          this.BALLS = _.concat(this.BALLS, {
+//        _.times(500, () => {
+//          this.BALLS = _.concat(this.BALLS, {
+//            x: _.random(this.size.width, true),
+//            y: _.random(this.size.height, true),
+//            radius: _.random(15, true),
+//            color: _.sample(['#c0392b', '#e67e22', '#f1c40f'])
+//          })
+//        })
+        while (this.BALLS.length < 150) {
+          let BALL = {
             x: _.random(this.size.width, true),
             y: _.random(this.size.height, true),
-            radius: _.sample([1, 2, 3, 4]),
-            vx: _.random(2, true) * _.sample([-1, 1]),
-            vy: _.random(2, true) * _.sample([-1, 1])
-          })
-        });
+            radius: _.random(15, true),
+            color: _.sample(['#c0392b', '#e67e22', '#f1c40f'])
+          };
+          if (this.BALLS.length === 0) {
+            this.BALLS = _.concat(this.BALLS, BALL)
+          }
+          for (let i = 0; i < this.BALLS.length; i++) {
+            let dx = this.BALLS[i].x - BALL.x;
+            let dy = this.BALLS[i].y - BALL.y;
+            let dr = this.BALLS[i].radius + BALL.radius;
+            if ((dx * dx + dy * dy) < dr * dr) {
+              break
+            } else if (i === this.BALLS.length - 1) {
+              this.BALLS = _.concat(this.BALLS, BALL)
+            }
+          }
+        }
       },
       renderArc(ctx){
         this.BALLS.map((ball) => {
           ctx.beginPath();
           ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-          ctx.fillStyle = 'rgb(255, 105, 3)';
+          ctx.fillStyle = ball.color;
           ctx.fill();
           ctx.closePath();
         })
